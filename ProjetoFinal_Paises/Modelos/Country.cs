a1f72fc47002f5e294d90caab93b96a92eb5ab8b
btadi <btadi@LEGION-DIOGO>
2023-05-12 19:32:57 +0100

    Add project files.

---
+++ ProjetoFinal_Paises/Modelos/Country.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal_Paises.Modelos;
+
+public class Country
+{
+    public Flag Flags { get; set; }
+    public CountryName Name { get; set; }
+    public string CCA3 { get; set; }
+    public bool UNMember { get; set; }
+    public Currency Currencies { get; set; }
+    public string[] Capital { get; set; }
+    public string Region { get; set; }
+    public string SubRegion { get; set; }
+    public Language Languages { get; set; }
+    public double[] LatLng { get; set; } = new double[2];
+    public string[] Borders { get; set; }
+    public string GoogleMaps { get; set; }
+    public int Population { get; set; }
+    public Gini Gini { get; set; }
+    public string[] Timezones { get; set; }
+    public string[] Continents { get; set; }
+
+}
---
+++ ProjetoFinal_Paises/Modelos/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal_Paises.Modelos;
+
+public class Response
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public object Result { get; set; }
+}
