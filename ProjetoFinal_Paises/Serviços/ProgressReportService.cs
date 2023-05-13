159354003a5b2e11f9525b4ab938be2cdbd9d8fb
_Hu5h <rubinhodavid@gmail.com>
2023-05-13 22:29:38 +0100

    implementei ProgressReportService.cs para usar IProgress

---
+++ ProjetoFinal_Paises/Serviços/ProgressReportService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ProjetoFinal_Paises.Modelos;
+
+namespace ProjetoFinal_Paises.Serviços;
+
+public class ProgressReportService
+{
+    public int Percentage { get; set; } = 0;
+
+    public List<Country> CountriesDownloaded { get; set; } = new List<Country>();
+}
