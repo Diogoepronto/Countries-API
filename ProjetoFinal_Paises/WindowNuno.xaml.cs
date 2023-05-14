a6523fc89d2741999df83bbb87944de3feab008c
Nuno Filipe de Vilhena Santos <nunovilhenasantos@msn.com>
2023-05-14 16:14:49 +0100

    adicionei mais janelas

---
+++ ProjetoFinal_Paises/WindowNuno.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ProjetoFinal_Paises.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetoFinal_Paises
+{
+    /// <summary>
+    /// Interaction logic for WindowNuno.xaml
+    /// </summary>
+    public partial class WindowNuno : Window
+    {
+        public WindowNuno()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ListBoxPaises_SelectionChanged(
+     object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCountry = (Country)ListBoxCountries.SelectedItem;
+
+            //DisplayCountryData(selectedCountry);
+        }
+    }
+}
